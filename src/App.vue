04647be8eeafa70490efda5cd65f1f115227dde8
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-17 09:34:51 +0200

    [ADD]: store e getApiTv

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,42 @@ import Header from './components/Header.vue';
 import Main from './components/Main.vue';
 import Search from './components/Search.vue';
 import axios from 'axios';
+import { store } from './data/store';
 export default {
 name: 'App',
 components: {
   Header,
   Search,
   Main
+},
+data() {
+  return {
+    store,
+  }
+},
+methods: {
+  getApiTv() {
+    const apiTv = store.apiUrl + store.tvSearch;
+    axios.get(apiTv, {
+      params:{
+        api_key: store.api_key,
+        query: store.query,
+        language: store.language
+      }
+    })
+    .then(res => {
+      store.tvResults = res.data.results;
+      console.log(store.tvResults);
+    })
+    .catch(e => {
+      console.log(e);
+    })
+  }
+},
+mounted() {
+  this.getApiTv();
 }
+
 }
 </script>
 
