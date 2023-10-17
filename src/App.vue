11415df83d745297f936ee876ee124e7040a3105
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-17 12:12:47 +0200

    [UPDATE]: template App component

--- src/App.vue
+++ src/App.vue
@@ -5,57 +5,58 @@ import Search from './components/Search.vue';
 import axios from 'axios';
 import { store } from './data/store';
 export default {
-name: 'App',
-components: {
-  Header,
-  Search,
-  Main
-},
-data() {
-  return {
-    store,
+  name: 'App',
+  components: {
+    Header,
+    Search,
+    Main
+  },
+  data() {
+    return {
+      store,
+    }
+  },
+  methods: {
+    getApiTv() {
+      const apiTv = store.apiUrl + store.tvSearch;
+      axios.get(apiTv, {
+        params: {
+          api_key: store.api_key,
+          query: store.query,
+          language: store.language
+        }
+      })
+        .then(res => {
+          store.tvResults = res.data.results;
+          console.log(store.tvResults);
+        })
+        .catch(e => {
+          console.log(e);
+        })
+    }
+  },
+  mounted() {
+    this.getApiTv();
   }
-},
-methods: {
-  getApiTv() {
-    const apiTv = store.apiUrl + store.tvSearch;
-    axios.get(apiTv, {
-      params:{
-        api_key: store.api_key,
-        query: store.query,
-        language: store.language
-      }
-    })
-    .then(res => {
-      store.tvResults = res.data.results;
-      console.log(store.tvResults);
-    })
-    .catch(e => {
-      console.log(e);
-    })
-  }
-},
-mounted() {
-  this.getApiTv();
-}
 
 }
 </script>
 
 
 <template>
+  <div class="d-flex bg-black">
 
-  <Header />
+    <Header />
 
-  <Search />
+    <Search />
+
+  </div>
+
 
   <Main />
-  
 </template>
 
 
 <style lang="scss">
-
 @use './scss/Main.scss'
-
 </style>
