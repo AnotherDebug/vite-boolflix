e2a0a305ad14bb7abccd100516fd2481a7bacd18
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-17 12:12:02 +0200

    [ADD]: image e style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,23 @@
 <script>
 export default {
-name: 'Header'
+    name: 'Header'
 }
 </script>
 
 
 <template>
-
- <header>Header</header>
-  
+    <header>
+        <img class="mx-5 my-3" :src="`../../public/logo-boolflix.png`" alt="">
+    </header>
 </template>
 
 
 <style lang="scss" scoped>
+header {
+    height: 70px;
 
+    img {
+        height: 60%;
+    }
+}
 </style>
