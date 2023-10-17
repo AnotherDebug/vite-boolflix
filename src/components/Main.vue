2648bf4900bccb7947437858a42514c6f40fb07b
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-17 09:34:06 +0200

    [ADD]: import card

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,17 +1,33 @@
 <script>
+import Card from './partials/card.vue';
+import { store } from '../data/store';
 export default {
-name: 'Main'
+    name: 'Main',
+    components: {
+        Card
+    },
+    data() {
+        return {
+            store
+        }
+    }
+
 }
 </script>
 
 
 <template>
+    <main>
+        <Card v-for="card in store.tvResults" 
+        :key="card.id"
+        :original_name="card.original_name"
+        :name = "card.name"
+        :original_language = "card.original_language"
+        :vote_average = "card.vote_average"
+        :overview = "card.overview" />
 
-    <main>Main</main>
-  
+    </main>
 </template>
 
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
