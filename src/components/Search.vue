9533bbd4071ac59aac224d40e20ddedba3a5eec2
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-17 15:44:07 +0200

    [ADD]: v model e style

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -13,13 +13,11 @@ export default {
 
 <template>
     <div class="container d-flex align-items-center justify-content-end">
-        <input v-model="store.query"  type="search" name="search" id="search" placeholder="Search...">
+        <input v-model="store.params.query"  type="search" name="search" id="search" placeholder="Search...">
 
-        <select class="form-select mx-4" style="width: 15%;" aria-label="Default select example">
-            <option selected>Open this select menu</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+        <select class="form-select mx-4"  aria-label="Default select example">
+            <option value="1">movie</option>
+            <option value="2">tv</option>
         </select>
 
         <button @click="$emit('startSearch')" class="btn btn-danger">Search</button>
@@ -27,4 +25,11 @@ export default {
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+select {
+    width: 100px;
+    min-width: 20px;
+}
+
+</style>
