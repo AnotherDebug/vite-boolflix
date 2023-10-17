197cd8594128f4cc5b791a869e9092fa2b3f737a
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-17 09:33:41 +0200

    [ADD]: card component

---
+++ src/components/partials/card.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+name: 'Card',
+props: {
+    original_name: String,
+    name:String,
+    original_language:String,
+    vote_average:String,
+    overview: String
+}
+}
+</script>
+
+
+<template>
+
+    <div>
+        <ul>
+            <li>{{ original_name }}</li>
+            <li>{{ name }}</li>
+            <li>{{ original_language }}</li>
+            <li>{{ vote_average }}</li>
+            <li>{{ overview }}</li>
+            <li>==========================</li>
+        </ul>
+    </div>
+  
+</template>
+
+
+<style>
+
+</style>
