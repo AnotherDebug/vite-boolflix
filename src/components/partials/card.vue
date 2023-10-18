a523316fe310427ac5366854d5df7e303888c40f
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-18 11:35:01 +0200

    [FIX]: Card

--- src/components/partials/card.vue
+++ src/components/partials/card.vue
@@ -3,12 +3,13 @@
 export default {
     name: 'Card',
     props: {
-        item: Object
+        item: Object,
+        // title: String
     },
     data() {
-        return{
+        return {
             imgPath: "https://image.tmdb.org/t/p/",
-            imgDimension : "w342",
+            imgDimension: "w342",
             languageFlags: {
                 en: '../../../public/en.png',
                 it: '../../../public/it.png'
@@ -16,7 +17,7 @@ export default {
         }
     },
     computed: {
-        voteStars(){
+        voteStars() {
             return Math.ceil(this.item.vote_average / 2);
         },
 
@@ -29,12 +30,32 @@ export default {
 
 
 <template>
+    <!-- <h1>{{ title }}</h1> -->
+
+    <div class="row m-auto my-3 ">
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img :src="`${imgPath}${imgDimension}` + item.poster_path" :alt="item.title || item.name">
+                <div class="card-body">
+                    <h5 class="card-title">{{ item.title || item.name }}</h5>
+                    <p class="card-text">{{ item.original_title || item.original_name }}</p>
+                    <img v-if="languageFlags.hasOwnProperty(item.original_language)"
+                        :src="languageFlags[item.original_language]" alt="">
+                    <p v-else>{{ `Lang: "${initialWords}"` }}</p>
+                    <p v-for="(n, index) in 5" :key="index" class="fa-star mt-4" :class="(n <= voteStars) ? 'fa-solid' : 'fa-regular' "></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
     <div class=" my-5 auto">
 
         <div class="wrapper">
 
             <!-- SINGOLA CARD -->
-            <div class="card">
+            <!-- <div class="card">
                 <img :src="`${imgPath}${imgDimension}`+ item.poster_path" :alt="item.title || item.name">
                 <div class="info">
                     <img v-if="languageFlags.hasOwnProperty(item.original_language)" :src="languageFlags[item.original_language]" alt="">
@@ -43,7 +64,7 @@ export default {
                     <p>{{ item.original_title || item.original_name }}</p>
                     <p v-for="(n, index) in 5" :key="index" class="fa-star" :class="(n <= voteStars) ? 'fa-solid' : 'fa-regular' "></p>
                 </div>
-            </div>
+            </div> -->
 
         </div>
 
@@ -52,80 +73,94 @@ export default {
 
 
 <style lang="scss" scoped>
-.wrapper {
-    display: flex;
-    flex-wrap: wrap;
-    width: 90%;
-    margin: auto;
 
-    .card {
-        width: 200px;
-        height: 300px;
-        padding: 0 20px;
-        background: white;
-        position: relative;
-        display: flex;
-        align-items: flex-end;
-        transition: 0.4s ease-out;
-        box-shadow: 0px 7px 10px rgba(black, 0.5);
-        margin: 10px;
-        margin-top: 100px;
-
-        &:hover {
-
-            &:before {
-                opacity: 1;
-            }
-
-            .info {
-                opacity: 1;
-                ;
+.row{
+    .col{
+        .card{
+            .card-body{
+                
+                img{
+                    display: block;
+                    height: 15px;
+                }
             }
         }
-
-        .info {
-            position: relative;
-            z-index: 3;
-            color: white;
-            opacity: 0;
-            transform: translateY(30px);
-            transition: 0.5s;
-
-            p {
-                letter-spacing: 1px;
-                font-size: 15px;
-                margin-top: 20px;
-            }
-            img {
-                height: 10px;
-                width: 20px;
-            }
-        }
-
-        img {
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-            position: absolute;
-            top: 0;
-            left: 0;
-        }
-
-        &:before {
-            content: "";
-            position: absolute;
-            top: 0;
-            left: 0;
-            display: block;
-            width: 100%;
-            height: 100%;
-            border-radius: 15px;
-            background: rgba(black, 0.6);
-            z-index: 2;
-            transition: 0.5s;
-            opacity: 0;
-        }
-
     }
 }
+// .wrapper {
+//     display: flex;
+//     flex-wrap: wrap;
+//     width: 90%;
+//     margin: auto;
+
+//     .card {
+//         width: 200px;
+//         height: 300px;
+//         padding: 0 20px;
+//         background: white;
+//         position: relative;
+//         display: flex;
+//         align-items: flex-end;
+//         transition: 0.4s ease-out;
+//         box-shadow: 0px 7px 10px rgba(black, 0.5);
+//         margin: 10px;
+//         margin-top: 100px;
+
+//         &:hover {
+
+//             &:before {
+//                 opacity: 1;
+//             }
+
+//             .info {
+//                 opacity: 1;
+//                 ;
+//             }
+//         }
+
+//         .info {
+//             position: relative;
+//             z-index: 3;
+//             color: white;
+//             opacity: 0;
+//             transform: translateY(30px);
+//             transition: 0.5s;
+
+//             p {
+//                 letter-spacing: 1px;
+//                 font-size: 15px;
+//                 margin-top: 20px;
+//             }
+//             img {
+//                 height: 10px;
+//                 width: 20px;
+//             }
+//         }
+
+//         img {
+//             width: 100%;
+//             height: 100%;
+//             object-fit: cover;
+//             position: absolute;
+//             top: 0;
+//             left: 0;
+//         }
+
+//         &:before {
+//             content: "";
+//             position: absolute;
+//             top: 0;
+//             left: 0;
+//             display: block;
+//             width: 100%;
+//             height: 100%;
+//             border-radius: 15px;
+//             background: rgba(black, 0.6);
+//             z-index: 2;
+//             transition: 0.5s;
+//             opacity: 0;
+//         }
+
+//     }
+//}
 </style>
